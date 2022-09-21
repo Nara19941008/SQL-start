711f9695582a27041975ff690ddd7a2bdbb8e4ae
Nara19941008 <narminim@code.edu.az>
2022-09-21 21:29:55 +0300

    Added bio table

---
+++ SQL-homework.sql
@@ -0,0 +1,23 @@
+--create database Company
+
+--use Company
+
+--create table Users(
+--[Id] int,
+--[Name] nvarchar(50),
+--[Surname] nvarchar(50),
+-- int ,
+-- nvarchar(50)
+--)
+
+--select * from Users
+
+--insert into Users([Id],[Name],[Surname],[Age],[Email])
+--values(1,'Narmin','Mammadova','28','narmin@gmail.com'),(2,'Fidan','Mardanli','21','fidan@gmail.com'),(3,'Saadet','Mamedova','17','saadet@gmail.com')
+
+--select * from Users where [age] < 20
+
+--insert into Users(Id,[Name],[Surname],[Age],[Email])
+--values(4,'Saadet','Mamedova','17','saadet@gmail.com')
+
+--select * from Users where [age] < 20
